c792a2b72599010abed0241715816beb6b12a3d2
kazuki <k_mikami@tokyoinst.co.jp>
2025-06-24 18:00:34 +0900

    add アイコン追加

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,5 @@
 import streamlit as st
 uploaded_file = st.file_uploader("アクセスログをアップロードしてください。")
+
+st.set_page_config(page_title="メインページ", page_icon='image/aurora.png')
+st.title("Multiple OSS Access Log Analyzer")
